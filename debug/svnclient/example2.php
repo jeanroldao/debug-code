e63221f807ae79fa7c894da396e55423a90ac3b8
jeanroldao <jeanfariasbr@hotmail.com>
2013-02-15 11:20:13 -0200

    websvn

--- debug/svnclient/example2.php
+++ debug/svnclient/example2.php
@@ -1,2 +1,2 @@
 <?php
-echo file_get_contents('http://jean:3139876@www.ditech.com.br/svn/DITECH/!svn/bc/18079//');
+echo file_get_contents('http://jean:'.base64_decode('MzEzOTg3Ng==').'@www.ditech.com.br/svn/DITECH/!svn/bc/18079//');
